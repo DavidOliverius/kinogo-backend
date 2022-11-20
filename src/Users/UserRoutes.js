0b8d5b07e4b67cfe95e5b19d48e089e075ff3af9
@Juliaoha <juliaoha@hotmail.com>
2022-11-20 19:00:15 +1100

    add error codes for frontend

--- src/Users/UserRoutes.js
+++ src/Users/UserRoutes.js
@@ -11,94 +11,100 @@ const {
 // Create a user, a session token & a refresh token
 routes.post("/sign-up", async (request, response) => {
   // Process posted form/json data
-  let newUserDetails = {
-    email: request.body.email,
-    password: request.body.password,
-    displayName: request.body.displayName,
-  };
   // Ideally perform validation on those properties before moving on.
   // Not in the scope of this guide though! ;)
-
+  const { email, password, displayName } = request.body;
+  const newUserDetails = {
+    email,
+    password,
+    displayName,
+  };
   // Hand data to a sign-up function
-  let signUpResult = await signUpUser({
+  const signUpResult = await signUpUser({
     email: newUserDetails.email,
     password: newUserDetails.password,
     displayName: newUserDetails.displayName,
   });
+
   // Return error or token as response
-  if (signUpResult.error != null) {
+  if (signUpResult.error || signUpResult.errorInfo) {
     console.log(
-      "Stopping the signup process due to an error. See logs for details."
+      `Stopping the signup process due to an error. ${signUpResult.error || signUpResult.errorInfo.message}`
     );
-    response.json(signUpResult);
-    return;
+    return response.status(401).json(signUpResult);
   }
 
   // Sign in to get latest user claims (authorization).
-  let signInResult = await signInUser({
+  const signInResult = await signInUser({
     email: newUserDetails.email,
     password: newUserDetails.password,
   });
 
   // If an error message exists, return that.
-  if (signInResult.error != null) {
+  if (signInResult.error) {
     console.log(
-      "Stopping the signup process due to an error. See logs for details."
+      `Stopping the signup process due to an error. ${signInResult.error}`
     );
-    response.json(signInResult);
-    return;
+    return response.status(401).json(signInResult);
   }
-
-  // On success, return a signed-in session to the brand-new user:
-  response.json(signInResult);
+  return response.status(201).json(signInResult);
 });
 
 // Create a session token & refresh token
 routes.post("/sign-in", async (request, response) => {
   // Process posted form/json data
-  let userDetails = {
-    email: request.body.email,
-    password: request.body.password,
-    // displayName: request.body.username,
+  const { email, password } = request.body;
+
+  const userDetails = {
+    email,
+    password,
   };
   // Ideally perform validation on those properties before moving on.
   // Not in the scope of this guide though! ;)
 
   // Hand data to a sign-in function
-  let signInResult = await signInUser({
+  const signInResult = await signInUser({
     email: userDetails.email,
     password: userDetails.password,
   });
 
   // If an error message exists, return that.
-  if (signInResult.error != null) {
+  if (signInResult.error) {
     console.log(
-      "Stopping the signup process due to an error. See logs for details."
+      `Stopping the signup process due to an error. ${signInResult.error}`
     );
-    response.json(signInResult);
-    return;
+    return response.status(401).json(signInResult);
   }
-
-  // On success, return a signed-in session to the brand-new user:
-  response.json(signInResult);
+  return response.status(201).json(signInResult);
 });
 
 // Create a session token & refresh token
 routes.post("/validate-session", async (request, response) => {
   // Process posted form/json data
-  let sessionDetails = {
-    idToken: request.body.idToken,
-    refreshToken: request.body.refreshToken,
+  const { idToken, refreshToken } = request.body;
+
+  const sessionDetails = {
+    idToken,
+    refreshToken,
   };
 
   // Hand data to a validation function
-  let validationResult = await validateUserSession({
+  const validationResult = await validateUserSession({
     refreshToken: sessionDetails.refreshToken,
     idToken: sessionDetails.idToken,
   });
 
-  // Return error or token as response
-  response.json(validationResult);
+  // If an error message exists, return that.
+  if (validationResult.error) {
+    console.log(
+      `Stopping the signup process due to an error. ${validationResult.error}`
+    );
+    return response.status(401).json(validationResult);
+  }
+  return response.status(200).json(validationResult);
+
+  // // Return error or token as response
+  // response.json(validationResult);
 });
 
 module.exports = routes;

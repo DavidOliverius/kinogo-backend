98a5fd4231003eb5c733b61bfa6805c551bc159c
DavidOliverius <99708980+DavidOliverius@users.noreply.github.com>
2022-11-21 17:31:58 +1100

    Update create new review, cleaned up comments

--- src/Reviews/ReviewsFunctions.js
+++ src/Reviews/ReviewsFunctions.js
@@ -1,6 +1,7 @@
 const { Review } = require("../database/schemas/ReviewsSchema");
+const { getDisplayNameFromUID } = require("../Users/UserFunctions");
 
-// Fina all reviews
+// Find all reviews
 async function getAllReviews() {
   let allReviews = await Review.find();
   return JSON.stringify(allReviews);
@@ -9,11 +10,12 @@ async function getAllReviews() {
 // Create new review
 async function createSpecificReview(reviewDetails) {
   let newReview = new Review({
-    reviewTitle: reviewDetails.reviewTitle,
     reviewContent: reviewDetails.reviewContent,
     reviewAuthorID: reviewDetails.reviewAuthorID,
     reviewApiID: reviewDetails.reviewApiID,
     rating: reviewDetails.rating,
+    // Grab username from UID and associate with review
+    user: await getDisplayNameFromUID(reviewDetails.reviewAuthorID),
   });
   let creationResult = await newReview.save();
   return creationResult;
@@ -36,13 +38,13 @@ async function updateSpecificReview(reviewID, reviewDetails) {
   return updateResult;
 }
 
-// Find all reviews with reviewAPI
+// Find all reviews with reviewApiID (title)
 async function getSpecificReviewID(reviewApiID) {
   let allReviews = await Review.find({ reviewApiID: reviewApiID });
   return JSON.stringify(allReviews);
 }
 
-// Find all reviews with reviewAuthorID
+// Find all reviews with reviewAuthorID (user)
 async function getSpecificReviewAuthorID(reviewAuthorID) {
   let allReviews = await Review.find({ reviewAuthorID: reviewAuthorID });
   return JSON.stringify(allReviews);
